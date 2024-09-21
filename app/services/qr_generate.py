56b6378b746b304a6f71f84fc54042a6d497925e
FireSpirit171 <rhodyrev7@gmail.com>
2024-09-21 19:42:52 +0300

    Генерация QR кода

--- app/services/qr_generate.py
+++ app/services/qr_generate.py
@@ -9,11 +9,11 @@ def get_qr(order_info, person_orders, person_price, total):
         modified_person_price[person] = person_price[i]
         i += 1
 
-    info = f"Заказ №{order_info['id']}\nСтол №{order_info['table']}\n\n"
+    info = f"Заказ №{order_info.id}\nСтол №{order_info.table_number}\n\n"
     for person, dishes in person_orders.items():
-        info += f'{person}:\n'
+        info += f'Гость №{person}:\n'
         for dish in dishes:
-            info += f"\t{dish[0]} - {dish[2]}шт. = {dish[1]}р.\n"
+            info += f"\t{dish[1]} - {dish[3]}шт. = {dish[2]}р.\n"
         info += f"\tСумма = {modified_person_price[person]}р.\n"
     info += f"Общая сумма = {total}р."
 
@@ -23,6 +23,8 @@ def get_qr(order_info, person_orders, person_price, total):
     buffer.seek(0)
     qr_image_base64 = base64.b64encode(buffer.read()).decode('utf-8')
 
-    return qr_image_base64
+    return f"data:image/png;base64,{qr_image_base64}"
+
+
 
 
