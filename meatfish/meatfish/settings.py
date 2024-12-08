5226691e9801ffd23948de55239bd618d3a055c6
FireSpirit171 <rhodyrev7@gmail.com>
2024-12-08 19:24:37 +0300

    change settings

--- meatfish/meatfish/settings.py
+++ meatfish/meatfish/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    # 'corsheaders',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    # 'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -155,11 +155,11 @@ REDIS_HOST = '127.0.0.1'
 REDIS_PORT = 6379
 SESSION_COOKIE_AGE = 1209600 
 
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:3000",
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "http://192.168.190.224:3000",
+]
 
-# CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_CREDENTIALS = True
 
 CSRF_COOKIE_NAME = 'csrftoken'
 CSRF_COOKIE_SECURE = False
